8c3bb350f3757a409c9cfdd13bf16467f6c194fc
zhuanqs <zhuanqingshan@163.com>
2013-01-14 16:13:07 +0800

    add ui

---
+++ NumberGuess/TWGuessViewController.h
@@ -0,0 +1,13 @@
+//
+//  TWGuessViewController.h
+//  NumberGuess
+//
+//  Created by 颛 清山 on 13-1-14.
+//  Copyright (c) 2013年 颛 清山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWGuessViewController : UIViewController
+
+@end
---
+++ TWViewController.m
@@ -0,0 +1,38 @@
+//
+//  TWViewController.m
+//  NumberGuess
+//
+//  Created by 颛 清山 on 13-1-14.
+//  Copyright (c) 2013年 颛 清山. All rights reserved.
+//
+
+#import "TWViewController.h"
+
+@interface TWViewController ()
+
+@end
+
+@implementation TWViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
